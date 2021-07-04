a720a5da381a9d7d30c115c6e0c8a645f43b2367
Miguel Pereira Medeiros <miguelpereiram@outlook.com>
2021-07-04 13:23:58 -0300

    :recycle: modify the way to change the badge style

--- src/atoms/badges/Badge.js
+++ src/atoms/badges/Badge.js
@@ -2,15 +2,21 @@ import React from "react";
 
 export default function Badge(props) {
     let styles = ' flex items-center justify-center font-medium rounded-full '
-    if(!props.width) styles += " max-w-min "
+    if (!props.width) styles += " max-w-min "
     switch (props.color) {
         case "red":
             styles += "bg-red-100 text-red-800 border border-red-300 "
+            break;
+        case "yellow":
+            styles += "bg-yellow-100 text-yellow-800 border border-red-300 "
             break;
         case "green":
             styles += "bg-green-100 text-green-800 border border-green-300"
             break;
         case "gray":
+            styles += "bg-gray-100 text-gray-800 border border-gray-300"
+            break;
+        default:
             styles += "bg-gray-100 text-gray-800 border border-gray-300"
             break;
     }
@@ -41,10 +47,9 @@ export default function Badge(props) {
             break;
     }
 
-    console.log(styles)
     return (
         <div className={`px-3 py-1 ${styles} ${props.tail}`}
-            style={{width:props.width, height:props.height}}>
+            style={{ width: props.width, height: props.height }}>
             {props.leftIcon}
             <p className="mx-1">{props.content}</p>
             {props.rightIcon}
