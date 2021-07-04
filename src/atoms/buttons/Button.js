be93e0dbd9fac003356c66935150e99016bdf797
Miguel Pereira Medeiros <miguelpereiram@outlook.com>
2021-07-04 13:21:08 -0300

    :lipstick: Add Button component

--- src/atoms/buttons/Button.js
+++ src/atoms/buttons/Button.js
@@ -1,12 +1,61 @@
 import React from "react";
 import PropTypes, { func } from "prop-types";
+import Strong from '../typography/Strong'
 
 export default function Button(props){
+
+    let styles = ""
+    switch (props.color){
+        case "red":
+            styles += " bg-red-600  text-white shadow"
+            break;
+        case "green":
+            styles +=  " bg-green-500  text-white shadow"
+            break;
+        default:
+            styles += "  text-gray-700 border border-gray-300"
+            break;
+    }
+
     return(
         <button onClick={() => props.click()}
-            className={` flex flex-shrink-0 flex-grow-0 h-auto w-auto py-2 px-4 border border-gray-100 rounded-lg shadow-sm hover:shadow-lg hover:scale-101 transition duration-300 ease-in-out items-center justify-around ${props.tail}`}
+            className={` rounded-md flex justify-center px-5 py-3 ${props.tail} ${styles} `}
             style={{width:props.width,height:props.height}}>
+              <Strong size={props.size}>
                 {props.children}
+              </Strong>
         </button>
     )
+  
 }
+
+/*
+
+return(
+        <button onClick={() => props.click()}
+            className={` flex justify-center px-5 py-3 border-gray-300 text-gray-700 rounded-md shadow ${props.tail}`}
+            style={{width:props.width,height:props.height}}>
+               {props.size === "xl" &&
+                    <Strong size={props.size}>
+                        {props.children}
+                    </Strong>
+               }
+               {props.size === "lg" &&
+                    <Strong size={props.size}>
+                        {props.children}
+                    </Strong>
+               }
+               {props.size === "sm" &&
+                    <Strong size={props.size}>
+                        {props.children}
+                    </Strong>
+               }
+               {props.size === "2xl" &&
+                    <Strong size={props.size}>
+                        {props.children}
+                    </Strong>
+               }
+        </button>
+    )
+
+*/
