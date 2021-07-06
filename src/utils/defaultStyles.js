142faff674f6f409dd32ff2db637cd0e250496a6
Miguel Pereira <miguelpereiram@outlook.com>
2021-07-06 16:54:38 +0000

    :construction: Add default values for the future modification of how to add styles to components

---
+++ src/utils/defaultStyles.js
@@ -0,0 +1,30 @@
+const defaultStyles = {
+    button:{
+        color:"gray",
+        size : "md",
+        outline : true,
+        bodyless : false,
+        dualtone: false,
+    },
+    badge:{
+        color:"green",
+        size : "xm",
+        outline : true,
+        bodyless : false,
+        dualtone: true,
+    },
+    container:{
+        color:"white",
+        size : "xm",
+        outline : true,
+        bodyless : false,
+        dualtone: false,
+    },
+    bar:{
+        color:"gray",
+        size : "xm",
+        outline : false,
+        bodyless : false,
+        dualtone: false,
+    }
+}
