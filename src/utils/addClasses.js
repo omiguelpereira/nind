7d28b8bd8a9990c0ae21ec9f27ec461f74495d48
Miguel Pereira <miguelpereiram@outlook.com>
2021-07-08 17:24:31 +0000

    Active propertiesVerifier function

--- src/utils/addClasses.js
+++ src/utils/addClasses.js
@@ -1,5 +1,5 @@
-import { avaibleParams } from "./avaibleParams";
-import { defaultStyles } from "./defaultStyles";
+import { avaibleParams } from "../constants/avaibleParams";
+import { defaultStyles } from "../constants/defaultStyles";
 import { propertiesVerifier } from "./verifierFunctions";
 import createStyleObject from "./createStyleObject";
 import { createClassString } from "./createClassString";
@@ -8,7 +8,7 @@ export default function addClasses(props,type){
 
     const customClasses = createStyleObject(props, type)
 
-    //propertiesVerifier(customClasses,avaibleParams).then(console.log)
+    propertiesVerifier(customClasses,avaibleParams).then(console.log)
     const ClassesString = createClassString(customClasses)
     if(!props.type) props.type = "default"
     const styles = ClassesString[props.type]
