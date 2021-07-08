e862ba84f6a3e52d41a2818e6d08eba7661c437d
Miguel Pereira <miguelpereiram@outlook.com>
2021-07-08 14:54:25 +0000

    Fixing error: Change === for =

--- src/utils/colorContrast.js
+++ src/utils/colorContrast.js
@@ -3,12 +3,12 @@ export default function colorContrast(contrastLevel = 4){
     if(contrastLevel > 9 || contrastLevel < 0){
         console.log("Invalid contrast level was past.")
         console.log("Using default values.")
-        return 4;
+        contrastLevel = 4;
     }
 
     for(let i = 0; i < 9; i++){
         if(contrastLevel === i){
-            contrastLevel === i*100
+            contrastLevel = i*100
             break
         }
     }
--- src/utils/colorIntensity.js
+++ src/utils/colorIntensity.js
@@ -2,14 +2,14 @@ export function ColorIntensity(intensityValue = 5, contrastLevel = 400 ){
 
     for(let i = 0; i < 9; i++){
         if(intensityValue === i){
-            intensityValue === i*100
+            intensityValue = i*100
             break
         }
     }
 
     if(intensityValue === 0) intensityValue = 50
 
-    while(intensityValue-contrastLevel < 50) contrastLevel-10
+    //while(intensityValue-contrastLevel < 50) contrastLevel-50
 
     let colors = {
         primary: intensityValue,
