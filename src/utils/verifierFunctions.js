231794bb1f44f74094b4e052a35458bd1df96936
Miguel Pereira <miguelpereiram@outlook.com>
2021-07-08 18:08:04 +0000

    Change the return object

--- src/utils/verifierFunctions.js
+++ src/utils/verifierFunctions.js
@@ -6,10 +6,9 @@ export function propertiesVerifier(properties, avaibleParams){
     for(let key in avaibleParams){
         if(!isaValidParam(properties[key],avaibleParams[key])){
             return {
-                sucesses: false,
                 message: `${properties[key]} is a invalid param`
             }
         }
     }
-    return {sucesses: true, message: "No invalid param found"}
+    return {message: "No invalid param found"}
 }
