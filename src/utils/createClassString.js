38413aaeb0f3ac6453bf594413e41f004b38e8e7
Miguel Pereira <miguelpereiram@outlook.com>
2021-07-08 14:57:56 +0000

    Change the way object properties are assigned

--- src/utils/createClassString.js
+++ src/utils/createClassString.js
@@ -1,8 +1,10 @@
 export function createClassString (styles) {
-    const alternativeStyles = {
-        outline: `text-${styles.colors.color}-${styles.colors.primary} border border-${styles.colors.color}-${styles.colors.primary}`,
-        bodyless: `text-${styles.colors.color}-${styles.colors.primary}`,
-        dualtone: `text-${styles.colors.color}-${styles.colors.primary} bg-${styles.colors.color}-${styles.colors.secundary}`
-    }
+    const alternativeStyles = {}
+    
+    alternativeStyles.outline = `text-${styles.colors.color}-${styles.colors.primary} border border-${styles.colors.color}-${styles.colors.primary}`
+    alternativeStyles.bodyless = `text-${styles.colors.color}-${styles.colors.primary}`
+    alternativeStyles.dualtone = `text-${styles.colors.color}-${styles.colors.primary} bg-${styles.colors.color}-${styles.colors.secundary}`
+    alternativeStyles.default = `text-white bg-${styles.colors.color}-${styles.colors.primary}`
+    
     return alternativeStyles
 }
