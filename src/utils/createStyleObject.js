65e0027c052904f9304c9ff04a5726ca6e7a43a0
Miguel Pereira <miguelpereiram@outlook.com>
2021-07-08 18:08:46 +0000

    Modify the imports

--- src/utils/createStyleObject.js
+++ src/utils/createStyleObject.js
@@ -1,5 +1,5 @@
-import { defaultStyles } from "./defaultStyles"
-import { ColorIntensity } from "./colorIntensity"
+import { defaultStyles } from "../constants/defaultStyles"
+import ColorIntensity from "./colorIntensity"
 import colorContrast from "./colorContrast"
 
 export default function createStyleObject(params,type){
@@ -9,7 +9,7 @@ export default function createStyleObject(params,type){
 
     let style = {}
     style.colors = {}
-    style.colors.color = params.color ? params.color : defaultStyles[type].colors.color
+    style.colors.color = params.color ? params.color : defaultStyles[type].colors.colorver  
     style.colors.primary = colors.primary
     style.colors.secundary = colors.secundary
     style.size = params.size ? params.size : defaultStyles[type].size
