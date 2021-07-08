3a01477664cef83423863abbdc3819fce7f9861c
Miguel Pereira <miguelpereiram@outlook.com>
2021-07-08 18:02:56 +0000

    add logs for verify the Classes

--- src/utils/addClasses.js
+++ src/utils/addClasses.js
@@ -2,13 +2,14 @@ import { avaibleParams } from "../constants/avaibleParams";
 import { defaultStyles } from "../constants/defaultStyles";
 import { propertiesVerifier } from "./verifierFunctions";
 import createStyleObject from "./createStyleObject";
-import { createClassString } from "./createClassString";
+import createClassString from "./createClassString";
 
 export default function addClasses(props,type){
 
     const customClasses = createStyleObject(props, type)
-
-    propertiesVerifier(customClasses,avaibleParams).then(console.log)
+    console.log(customClasses)
+    const result = propertiesVerifier(customClasses,avaibleParams)
+    console.warn(result)
     const ClassesString = createClassString(customClasses)
     if(!props.type) props.type = "default"
     const styles = ClassesString[props.type]
