5931a13ed1c193b60caea86a9e95f11b07792b23
Miguel Pereira <miguelpereiram@outlook.com>
2021-07-12 14:17:29 +0000

    :construction: Export IconBadge

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export {default as Button} from "./atoms/buttons/Button";
 export {default as Container} from "./atoms/containers/Container";
 export {default as Badge} from './atoms/badges/Badge'
 export {default as Bar} from './atoms/bar/Bar'
+export {default as IconBadge} from './atoms/iconBadge/IconBadge'
