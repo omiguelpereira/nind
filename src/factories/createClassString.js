7f745624e96877a7c74cce2a1ee92e0c3d27cca2
Miguel Pereira <miguelpereiram@outlook.com>
2021-07-12 13:51:45 +0000

    :lipstick: Add text size in class creation

--- src/factories/createClassString.js
+++ src/factories/createClassString.js
@@ -3,7 +3,7 @@ export default function createClassString (styles) {
     
     alternativeStyles.outline = `text-${styles.colors.color}-${styles.colors.primary} border border-${styles.colors.color}-${styles.colors.primary}`
     alternativeStyles.bodyless = `text-${styles.colors.color}-${styles.colors.primary}`
-    alternativeStyles.dualtone = `text-${styles.colors.color}-${styles.colors.primary} bg-${styles.colors.color}-${styles.colors.secundary}`
+    alternativeStyles.dualtone = `text-${styles.colors.color}-${styles.colors.primary} bg-${styles.colors.color}-${styles.colors.secundary} ${styles.size ? `text-${styles.size}` : ""}`
     alternativeStyles.default = `text-white bg-${styles.colors.color}-${styles.colors.primary} ${styles.size ? `text-${styles.size}` : ""}`
 
     return alternativeStyles
