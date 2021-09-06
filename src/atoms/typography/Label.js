2051dc8d9f285bf9ad5c097939625095c98d7dbb
Miguel Pereira Medeiros <miguelpereiram@outlook.com>
2021-09-05 21:16:25 -0300

    :recycle: Rename component name

--- src/atoms/typography/Label.js
+++ src/atoms/typography/Label.js
@@ -1,11 +1,11 @@
 import React from "react";
 import addClasses from "../../utils/addClasses";
-export default function Strong(props) {  
+export default function Label(props) {  
     
     const styles = addClasses(props,"label")
 
     return(
-        <p className={`flex flex-row gap-1 leading-5 font-medium ${props.tail} ${styles}`}>
+        <p className={`leading-2 ${props.tail} ${styles}`}>
             {props.children}
         </p>
     )
