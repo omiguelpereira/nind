738a8f7ed174a7ca19858b85e3d3bc63ba94e15f
Miguel Pereira Medeiros <miguelpereiram@outlook.com>
2021-09-05 21:15:40 -0300

    :recycle: Refactors string interpolation to work with new structure

--- src/factories/createClassString.js
+++ src/factories/createClassString.js
@@ -2,12 +2,12 @@ export default function createClassString (styles) {
     
     const alternativeStyles = {}
     
-    alternativeStyles.outline = `text-${styles.colors.color}-${styles.colors.primary} border border-${styles.colors.color}-${styles.colors.primary}`
-    alternativeStyles.bodyless = `text-${styles.colors.color}-${styles.colors.primary}`
-    alternativeStyles.dualtone = `text-${styles.colors.color}-${styles.colors.primary} bg-${styles.colors.color}-${styles.colors.secundary} }`
-    alternativeStyles.default = `text-white bg-${styles.colors.color}-${styles.colors.primary} `
+    alternativeStyles.outline = `text-${styles.color}-${styles.colorIntensity.strongTone} border border-${styles.color}-${styles.colorIntensity.primary}`
+    alternativeStyles.bodyless = `text-${styles.color}-${styles.colorIntensity.strongTone}`
+    alternativeStyles.dualtone = `text-${styles.color}-${styles.colorIntensity.strongTone} bg-${styles.color}-${styles.colorIntensity.weakerTone} }`
+    alternativeStyles.default = `bg-${styles.color}-${styles.colorIntensity.strongTone} `
 
-    if(styles.type === "typographic") return alternativeStyles[styles.fill].concat(` text-${styles.font.size} font-${styles.colors.weight} tracking-${styles.colors.spacing} `)
+    if(styles.type === "typographic") return alternativeStyles[styles.fill].concat(` text-${styles.font.size} font-${styles.font.weight} tracking-${styles.font.spacing} `)
 
     return alternativeStyles[styles.fill]
 
