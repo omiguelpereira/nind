5b6e8cde839f4eb5c3d08c09b03dddb4d6a89317
Miguel Pereira Medeiros <miguelpereiram@outlook.com>
2021-09-05 21:14:26 -0300

    :building_construction: Changes the object structure of allowed parameters

--- src/constants/alternativeStyles.js
+++
@@ -1,1 +0,0 @@
-export const alternativeStyles = ["outline","bodyless","dualtone"]
--- src/constants/avaibleParams.js
+++ src/constants/avaibleParams.js
@@ -1,4 +1,17 @@
-export const avaibleParams = {
+export const defaultParams = {
     size: ["xs","sm","md","base","lg","xl","2xl"],
-    color: ["green","red","yellow","gray"]
+    color: ["green","red","yellow","gray"],
+    fill: ["outline","bodyless","dualtone", "bodied"],
+    strongTone: [50,100,200,300,400,500,600,700,800,900],
+    weakerTone: [50,100,200,300,400,500,600,700,800,900],
+    weight: ["normal","bold"],
+    spacing: ["normal","wide"]
+}
+
+export const alternativeParams = {
+    extrasmall: "xs",
+    small: "sm",
+    medium: "md",
+    large: "lg",
+    extralarge: "xl"
 }
